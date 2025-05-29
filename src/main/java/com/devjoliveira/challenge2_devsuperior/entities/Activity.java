27d24a3f0a9ec5edf756a2d7886aac61fbcd32cf
devjoliveira <macpro@MacBook-Pro-de-MacPro.local>
2025-05-28 22:39:26 -0300

    Create Participante, Category and ORM

---
+++ src/main/java/com/devjoliveira/challenge2_devsuperior/entities/Activity.java
@@ -0,0 +1,10 @@
+package com.devjoliveira.challenge2_devsuperior.entities;
+
+public class Activity {
+
+  private Integer id;
+  private String name;
+  private String description;
+  private Double price;
+
+}
