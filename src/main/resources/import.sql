e0d74b678f970893f112c770622d54cfc06f8b29
devjoliveira <macpro@MacBook-Pro-de-MacPro.local>
2025-05-28 22:47:38 -0300

    Create Seed tb_participant

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_participant(name, email) VALUES ('José Silva', 'jose@gmail.com');
+INSERT INTO tb_participant(name, email) VALUES ('Tiago Maria', 'tiago@gmail.com');
+INSERT INTO tb_participant(name, email) VALUES ('Maria do Rosário', 'maria@gmail.com');
+INSERT INTO tb_participant(name, email) VALUES ('Teresa da Silva', 'teresa@gmail.com');
